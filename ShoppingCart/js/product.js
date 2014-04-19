adcb060a2025bc20f0c4c9edfd22569d16bc3ee6
Alexander Bogardi <alexanderbogardi@macintosh-3.lan>
2014-04-19 23:50:27 +0200

    pictures

--- ShoppingCart/js/product.js
+++ ShoppingCart/js/product.js
@@ -7,4 +7,5 @@ function product(sku, name, description, price, sizes, colors) {
     this.price = price;
     this.sizes = sizes;
     this.colors = colors;
+    this.colorCodes = {"brown":19,"yellow":30,"cerise":60,"green":72,"blue":89,"red":99}; //move this to an own static entity
     }
--- ShoppingCart/js/store.js
+++ ShoppingCart/js/store.js
@@ -11,8 +11,8 @@
 //
 function store() {
     this.products = [
-        new product("SK1", "Jumbo", "Pork in new shoes", 100,  [20,21,25,26,27],["blue","green"]),
-        new product("SK2", "Mambo", "Great", 200, [19,26,27,33,36],["violet","pink"])
+        new product("99841", "Madicken", "language.madicken", 649,  [20,21,25,26,27],["yellow","cerise","green","blue","red"]),
+        new product("93541", "Mimer", "language.mimer", 699, [19,26,27,33,36],["brown","blue","red"])
     ];
 }
 store.prototype.getProduct = function (sku) {
