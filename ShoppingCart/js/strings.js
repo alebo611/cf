d38eb0346c4f06795c76c760d69d803ed6166793
Alexander Bogardi <alexanderbogardi@macintosh-3.lan>
2014-04-29 23:30:26 +0200

    currency filter

--- ShoppingCart/js/strings.js
+++ ShoppingCart/js/strings.js
@@ -1,16 +1,19 @@
 var swedish = {welcome:'Välkommen',
                 description:'Detta är en produkt',
+                currency:'SEK',
                 search:'Sök',
                 madicken:'Sommarballerina i härliga färgkombinationer. Ovanläder i svenskt vegetabilgarvat skinn helt utan skadliga tillsatser. Justerbar kardborreknäppning och fotriktig läst gör att skon passar de allra flesta fötter. Invändigt förstärkt både fram i tån och bak i hälen. Urtagbar innersula i skinn, underliggande och stöttdämpande mellansula och yttersula med bra grepp och avrullning. Certifierad med EU eco-label',
                 mimer:'Mimer svenska'
 };
 var english = {welcome:'Welcome',
+                currency:'SEK',
                 description:'This is a product',
                 search:'Search',
                 madicken:'Madicken english',
                 mimer:'Mimer english'
 };
 var chinese = {welcome:'欢迎',
+                currency:'RMB',
                 description:'你好',
                 search:'搜索',
                 madicken:'Madicken chinese',
