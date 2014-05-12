c92b4359732253e9f6367441e5edfa33e7d18f8f
Alexander Bogardi <alexanderbogardi@macintosh-3.lan>
2014-05-12 23:20:35 +0200

    added more shoes. The layout needs to change so it becomes fixed. Well thats for another day fer sure.

--- ShoppingCart/js/store.js
+++ ShoppingCart/js/store.js
@@ -33,7 +33,34 @@ function store($http) {
 
     this.products = [
         new product("99841", "Madicken", 'madicken', 649,  [20,21,25,26,27],["green","cerise","yellow","blue","red"]),
-        new product("93541", "Mimer", 'mimer', 699, [19,26,27,33,36],["brown","blue","red"])
+        new product("93541", "Mimer", 'mimer', 699, [19,26,27,33,36],["brown","blue","red"]),
+        new product("65041", "Alvin", 'alvin', 699, [19,26,27,33,36],["blue","red"]),
+        new product("99641", "Maja", 'maja', 699, [19,26,27,33,36],["light-brown","red"]),
+        new product("92341", "Amanda", 'amanda', 699, [19,26,27,33,36],["light-brown","red"]),
+        new product("10041", "Anders", 'anders', 699, [19,26,27,33,36],["blue","red"]),
+        new product("66841", "Molly", 'molly', 699, [19,26,27,33,36],["white","red"])
+
+
+/*
+bengt-10241-19-brown.png		myra-92741-60-cerise.png
+billy-63241-99-red.png			nanna-98441-99-red.png
+blixten-64241-60-cerise.png		navran-83141-89-blue-(big).png
+bo-99341-19-brown.png			no1-21041-93-multi.png
+bosse-10441-19-brown.png		prick-71641-72-green.png
+caroline-93741-89-blue.png		ralf-64141-19-brown.png
+charlie-66741-11-black.png		regn-63841-85-turquoise.png
+emma-60241-79-pink.png			robin-99441-89-blue.png
+erik-42241-11-black.png			ronnerdal-blue.png
+fjaril-97941-19-brown.png		rudolf-10541-89-blue.png
+hasse-11141-39-light-brown.png		sam-98241-72-green.png
+hjördis-92441-39-light-brown.png	sigge-60341-11-black.png
+kotte-90441-85-turqoise.png		spruttan-cerise.png
+lasse-10641-19-brown.png		stina-cerise.png
+lina-96641-30-yellow.png		svea-22241-79-pink.png
+linnea-67041-60-cerise.png		sydney-82341-60-cerise.png
+lotta-93841-39-light-brown.png		tor-91141-39-light-brown.png
+madicken-99841-72-green.png
+*/
     ];
 }
 store.prototype.getProduct = function (sku) {
